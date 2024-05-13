605625e4634a7e3e75948a84383d37f4c816d367
Ilia Shved <ilia83ivanovich@gmail.com>
2024-05-13 15:00:14 +0500

    unit_1.8

--- unit_01/8/unit_08.js
+++ unit_01/8/unit_08.js
@@ -1,20 +1,29 @@
 // Task 1 ============================================
 /* Заполните массив целыми числами от 1 до 10.*/
-let a1 = 565;
+let a1 = [];
 
 function t1() {
-  let str = String(a1);
-  document.querySelector('.out-1').innerHTML = str[0];
+  for (let i = 0; i < 10; i++) {
+    a1.push(i + 1);
+  }
+  document.querySelector('.out-1').innerHTML = a1;
 }
 document.querySelector('.b-1').addEventListener('click', t1);
 
 // Task 2============================================
 /* Заполните массив четными числами из промежутка от 1 до 100.*/
-let a2 = 569848;
+let a2 = [];
 
 function t2() {
-  let str = String(a2);
-  document.querySelector('.out-2').innerHTML = str[str.length - 1];
+  a2 = [];
+  while (a2.length < 20) {
+    let num = Math.round(Math.random() * 100);
+    if (num % 2 == 0) {
+      a2.push(num);
+    }
+  }
+  console.log(a2);
+  document.querySelector('.out-2').innerHTML = a2;
 }
 document.querySelector('.b-2').addEventListener('click', t2);
 
@@ -22,10 +31,15 @@ document.querySelector('.b-2').addEventListener('click', t2);
 /* Дан массив с дробями:
 [1.456, 2.125, 3.32, 4.1, 5.34]
 Округлите эти дроби до одного знака в дробной части.*/
-let a3 = 569848;
+let a3 = [1.456, 2.125, 3.32, 4.1, 5.34];
 
-function t2() {
-  let str = String(a2);
-  document.querySelector('.out-2').innerHTML = str[str.length - 1];
+function t3() {
+  let arr = [];
+  for (let item of a3) {
+    arr.push(item.toFixed(1));
+  }
+  a3 = arr;
+  console.log(a3);
+  document.querySelector('.out-3').innerHTML = a3;
 }
-document.querySelector('.b-2').addEventListener('click', t2);
+document.querySelector('.b-3').addEventListener('click', t3);
