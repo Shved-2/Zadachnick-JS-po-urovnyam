b7df33b03e389de41dd73006c9fde602c8a1810c
Ilia Shved <ilia83ivanovich@gmail.com>
2024-05-13 13:11:23 +0500

    unit_1.6

--- unit_01/6/unit_06.js
+++ unit_01/6/unit_06.js
@@ -1,39 +1,56 @@
 // Task 1 ============================================
 /*  Дан массив с числами. Найдите сумму квадратов элементов этого массива.*/
-let a1 = 565;
+let arr = [6, 5, 8, 2, -9, 45, -8, 6, 12, 10, -6, 8, 9, 1, -3, -4];
 
 function t1() {
-  let str = String(a1);
-  document.querySelector('.out-1').innerHTML = str[0];
+  let summ = 0;
+  for (let item of arr) {
+    summ += item * item;
+  }
+
+  document.querySelector('.out-1').innerHTML = summ;
 }
 document.querySelector('.b-1').addEventListener('click', t1);
 
 // Task 2============================================
 /* Дан массив с числами. Найдите сумму квадратных корней элементов этого массива.*/
-let a2 = 569848;
-
+let arr2 = [6, 5, 8, 2, 9, 45, 8, 6, 12, 10, 6, 8, 9, 1, 3, 4];
 function t2() {
-  let str = String(a2);
-  document.querySelector('.out-2').innerHTML = str[str.length - 1];
+  let summ = 0;
+  for (let item of arr2) {
+    summ += Math.sqrt(item);
+  }
+
+  document.querySelector('.out-2').innerHTML = summ;
 }
 document.querySelector('.b-2').addEventListener('click', t2);
 
 // Task 3============================================
 /* Дан массив с числами. Найдите сумму положительных элементов этого массива.*/
-let a3 = 569848;
 
-function t2() {
-  let str = String(a2);
-  document.querySelector('.out-2').innerHTML = str[str.length - 1];
+function t3() {
+  let summ = 0;
+
+  for (let item of arr) {
+    if (item % 2 == 0) {
+      summ += item;
+    }
+  }
+  document.querySelector('.out-3').innerHTML = summ;
 }
-document.querySelector('.b-2').addEventListener('click', t2);
+document.querySelector('.b-3').addEventListener('click', t3);
 
 // Task 4 ============================================
 /* Дан массив с числами. Найдите сумму тех элементов этого массива, которые больше нуля и меньше десяти.*/
-let a4 = 565;
+let arr4 = [6, 5, 8, 2, -9, 45, -8, 6, 12, 10, -6, 8, 19, 1, -3, -4];
+function t4() {
+  let summ = 0;
 
-function t1() {
-  let str = String(a1);
-  document.querySelector('.out-1').innerHTML = str[0];
+  for (let item of arr4) {
+    if (item > 0 && item < 10) {
+      summ += item;
+    }
+  }
+  document.querySelector('.out-4').innerHTML = summ;
 }
-document.querySelector('.b-1').addEventListener('click', t1);
+document.querySelector('.b-4').addEventListener('click', t4);
