9c5f6bc0c834689ca641e3ea7653be054e2c836f
Ilia Shved <ilia83ivanovich@gmail.com>
2024-05-13 22:35:05 +0500

    unit_1.9

--- unit_01/9/unit_09.js
+++ unit_01/9/unit_09.js
@@ -1,29 +1,62 @@
 // Task 1 ============================================
 /*  Дан массив со строками. Оставьте в этом массиве только те строки, которые начинаются на http://.*/
-let a1 = 565;
+let a1 = [
+  'http://code.mu/',
+  'https://www.youtube.com/',
+  'https://e.mail.ru/',
+  'http://2books.su/tags/adapted/',
+  'http://translate.google.com/',
+  'https://tproger.ru/curriculum/php-beginner/',
+];
 
 function t1() {
-  let str = String(a1);
-  document.querySelector('.out-1').innerHTML = str[0];
+  let arr = [];
+  for (let item of a1) {
+    if (item.includes('http://')) {
+      arr.push(item);
+    }
+  }
+  document.querySelector('.out-1').innerHTML = arr;
+  console.log(arr);
 }
 document.querySelector('.b-1').addEventListener('click', t1);
 
 // Task 2============================================
 /* Дан массив со строками. Оставьте в этом массиве только те строки, которые заканчиваются на .html.*/
-let a2 = 569848;
+let a2 = [
+  'index.html',
+  'index.php',
+  'logo.svg',
+  'yandex.com',
+  'main.html',
+  'list.js',
+  'about.html',
+  'name.js',
+  'list.php',
+  'cat.jpg',
+];
 
 function t2() {
-  let str = String(a2);
-  document.querySelector('.out-2').innerHTML = str[str.length - 1];
+  let arr = [];
+  for (let item of a2) {
+    if (item.endsWith('.html')) {
+      arr.push(item);
+    }
+  }
+  document.querySelector('.out-2').innerHTML = arr;
+  console.log(arr);
 }
 document.querySelector('.b-2').addEventListener('click', t2);
 
 // Task 3============================================
 /* Дан массив с числами. Увеличьте каждое число из массива на 10 процентов.*/
-let a3 = 569848;
+let a3 = [1, 54, 32, 9, -9, 10, 33, 11, 10, -8, -7, 2, 7, 45];
 
-function t2() {
-  let str = String(a2);
-  document.querySelector('.out-2').innerHTML = str[str.length - 1];
+function t3() {
+  let arr = a3.map((elem) => {
+    return ((elem / 100) * 110).toFixed(1);
+  });
+  document.querySelector('.out-3').innerHTML = arr;
+  console.log(arr);
 }
-document.querySelector('.b-2').addEventListener('click', t2);
+document.querySelector('.b-3').addEventListener('click', t3);
