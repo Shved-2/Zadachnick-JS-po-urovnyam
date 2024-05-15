a2f28e723dbdd84717711491b550efb2c8fdcef9
Ilia Shved <ilia83ivanovich@gmail.com>
2024-05-16 00:18:39 +0500

    unit_2.9

--- unit_02/09/unit_09.js
+++ unit_02/09/unit_09.js
@@ -1,9 +1,21 @@
 // Task 1 ============================================
 /*  Дано некоторое число: 123456 Найдите сумму пар цифр этого числа. В нашем случае имеется
   ввиду следующее: 12 + 34 + 56*/
-let a1 = [1, 2, 56, -6, 7, -2, -8, 12, -45, 78, -12, -45, -8, 41, 15, 18, -3];
+let a1 = 123456;
 
-function t1() {}
+function t1() {
+  let arr = String(a1).split('');
+  let arr2 = [];
+  let str = 0;
+  for (let i = 0; i < arr.length; i += 2) {
+    arr2.push(arr[i] + arr[i + 1]);
+  }
+  for (let item of arr2) {
+    str += Number(item);
+  }
+  document.querySelector('.out-1').innerHTML = str;
+  console.log(str);
+}
 document.querySelector('.b-1').addEventListener('click', t1);
 
 // Task 2============================================
@@ -11,7 +23,11 @@ document.querySelector('.b-1').addEventListener('click', t1);
 
 [1, 2, 3, 4, 5]
 Выведите в консоль элементы этого массива в обратном порядке.*/
-let a2 = [1, 2, 56, -6, 7, -2, -8, 12, -45, 78, -12, -45, -8, 41, 15, 18, -3];
+let a2 = [1, 2, 3, 4, 5];
 
-function t2() {}
+function t2() {
+  let arr = a2.reverse();
+  document.querySelector('.out-2').innerHTML = arr;
+  console.log(arr);
+}
 document.querySelector('.b-2').addEventListener('click', t2);
--- unit_02/10/unit_10.js
+++ unit_02/10/unit_10.js
@@ -1,19 +1,20 @@
 // Task 1 ============================================
 /*  Дана строка с буквами и цифрами. Проверьте, что в этой строке не более трех букв.*/
-
+let i = document.querySelector('.i-1');
 function t1() {}
 document.querySelector('.b-1').addEventListener('click', t1);
 
 // Task 2============================================
 /*  Дано число. Получите первую четную цифру с конца этого числа*/
 
+let num2 = document.querySelector('.i-2');
 function t2() {}
 document.querySelector('.b-2').addEventListener('click', t2);
 
 // Task 3============================================
 /*  Дана некоторая строка: 'abcde abcde abcde' Замените в ней первый символ каждого слова на
    '!': '!bcde !bcde !bcde'*/
-let a3 = '12,34,56';
+let a3 = 'abcde abcde abcde';
 function t3() {}
 document.querySelector('.b-3').addEventListener('click', t3);
 
@@ -21,6 +22,6 @@ document.querySelector('.b-3').addEventListener('click', t3);
 /*  Дан массив с числами: [1, 2, 3, 3, 4, 5] Проверьте, что в этом массиве есть два одинаковых
   элемента подряд*/
 
-let a4 = '2025-12-31';
+let a4 = [1, 2, 3, 3, 4, 5];
 function t4() {}
 document.querySelector('.b-4').addEventListener('click', t4);
